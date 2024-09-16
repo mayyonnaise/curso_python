1a297181fedcc7f6d2e232285ca280df5ae7fcd7
nick <nicollcrhisosthemosteixeira@gmail.com>
2024-09-16 18:54:21 -0300

    Add files via upload

---
+++ desafio2.py
@@ -0,0 +1,8 @@
+'''
+dia=input('dia:')
+mes=input('mes:')
+ano=input('ano:')
+print('você nasceu no dia '+dia+ " de " +mes+ " de " +ano)
+'''
+nome=input('qual é o seu nome?')
+print('seu nome é '+nome+', prazer em te conhecer')
