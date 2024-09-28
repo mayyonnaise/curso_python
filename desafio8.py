06f91f7c5cd7ee174caa259ecaf7302b768bba0b
nick <nicollcrhisosthemosteixeira@gmail.com>
2024-09-28 12:07:21 -0300

    Add files via upload

---
+++ desafio8.py
@@ -0,0 +1,7 @@
+var = input('escreva um valor em metros: ')
+
+metro = float(var)
+centimetro = metro * 100
+milimetro = metro * 1000
+
+print('o valor de {} metros é igual a {} centimetros e {} em milimetros'.format(metro , centimetro , milimetro))
