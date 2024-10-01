4d99a9d67f973e1379e8e354f55f087e834a8434
nick <nicollcrhisosthemosteixeira@gmail.com>
2024-10-01 11:23:57 -0300

    Add files via upload

---
+++ desafio11.py
@@ -0,0 +1,4 @@
+larg = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
+área = larg * alt
+print('A sua parede tem a dimensão de {}x{} e sua área é de {}m²'.format(larg, alt, área))
